215a6e7aa715981f8da6e41c33ebd3f70601d163
miltondavalos <miltondavalos@gmail.com>
2014-06-01 00:48:20 -0400

    refactor

--- main.rb
+++ main.rb
@@ -5,30 +5,6 @@ require "sinatra/reloader" if development?
 
 set :sessions, true
 
-def calculate_total(cards)
-  # [['H', '3'], ['S', 'Q'], ... ]
-  arr = cards.map{|e| e[1] }
-
-  total = 0
-  arr.each do |value|
-    if value == "A"
-      total += 11
-    elsif value.to_i == 0 # J, Q, K
-      total += 10
-    else
-      total += value.to_i
-    end
-  end
-
-  #correct for Aces
-  arr.select{|e| e == "A"}.count.times do
-    total -= 10 if total > 21
-  end
-
-  total
-end
-
-
 helpers do
   def new_deck
     suits = ['H', 'D', 'S', 'C']
@@ -57,10 +33,40 @@ helpers do
       arr.select{|e| e == "A"}.count.times do
         total -= 10 if total > 21
       end
-
       total
-    end
   end
+
+  def display_cards(cards)
+    suit = cards[0]
+    card = cards[1]
+    case suit
+      when 'C'
+        suit = 'clubs'
+      when 'D'
+        suit='diamonds'
+      when 'H'
+        suit='hearts'
+      when 'S'
+        suit='spades'
+    end
+
+    case card
+      when 'K'
+        card = 'king'
+      when 'Q'
+        card = 'queen'
+      when 'J'
+        card = 'jack'
+      when 'A'
+        card = 'ace'
+    end
+    "#{suit}_#{card}.jpg"
+  end
+end
+
+before do
+  @show_hit_or_stay_buttons = true
+end
 #route that renders text in the browser
 get '/home' do
   'Welcome Home!'
@@ -84,11 +90,15 @@ end
 get '/game' do
   session[:player_cards] = []
   session[:dealer_cards] = []
+  session[:dealer_cards_suit] = []
   session[:deck] = new_deck
   session[:player_cards] << session[:deck].pop
   session[:dealer_cards] << session[:deck].pop
+  #session[:dealer_cards_suit] << display_cards(session[:dealer_cards])
   session[:player_cards] << session[:deck].pop
   session[:dealer_cards] << session[:deck].pop
+  session[:dealer_cards_suit] << display_cards(session[:dealer_cards])
+  session[:dealer_total] = calculate_total(session[:dealer_cards])
   session[:player_total] = calculate_total(session[:player_cards])
   erb :game
 end
@@ -107,32 +117,55 @@ end
 
 get '/new_game' do
   erb :clear_sessions
-#  #redirect '/'
   erb :new_game
 end
 
-#get '/new_game' do
-#  if session[:username]
-#    redirect '/game'
-#  else
-#    erb :new_game
-#  end
-#end
-
 post '/new_game' do
   session[:username] = params[:username]
-  #erb :new_game
   redirect '/game'
 end
 
 post '/game/player/hit' do
+  #session[:player_cards] << session[:deck].pop
+  #session[:player_total] = calculate_total(session[:player_cards])
+  #erb :game
   session[:player_cards] << session[:deck].pop
-  session[:player_total] = calculate_total(session[:player_cards])
+  if calculate_total(session[:player_cards]) > 21
+    @error = "You busted. Got #{calculate_total(session[:player_cards])}!"
+    @show_hit_or_stay_buttons = false
+  end
   erb :game
 end
 
+#get '/game/player/stay' do
+#  session[:dealer_total] = calculate_total(session[:dealer_cards])
+#
+#  while session[:dealer_total] < 17
+#    session[:dealer_cards] << session[:deck].pop
+#    session[:dealer_total] = calculate_total(session[:dealer_cards])
+#    erb :game
+#  end
+#end
+
 post '/game/player/stay' do
+  @success = "You have chosen to stay"
+  @show_hit_or_stay_buttons = false
+  session[:dealer_total] = calculate_total(session[:dealer_cards])
+
+  while session[:dealer_total] < 17
+    session[:dealer_cards] << session[:deck].pop
+    session[:dealer_total] = calculate_total(session[:dealer_cards])
+    #erb :game
+  end
   erb :game
+
+  #if session[:dealer_total] > 21
+  #  "Dealer busted #{session[:dealer_total]}."
+  #  erb :game
+  #else
+  #  "Dealer: #{session[:dealer_total]} vs. #{session[:username]}: #{session[:player_total]}"
+  #  erb :game
+  #end
 end
 
 get '/game_over' do
